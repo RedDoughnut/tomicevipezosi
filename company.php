79926f8db0b8f852e7ee1411dcde47ea5aa61261
David Tomić <80597004+Programmers3001@users.noreply.github.com>
2024-12-22 23:28:31 +0100

    utf8

--- company.php
+++ company.php
@@ -4,6 +4,7 @@
      <head>
         <title>Kompanija</title>
         <link rel="stylesheet" href="nav.css">
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
      </head>
      <style>
