d90e03c263c1283950e01dc230a48a7177d37371
scriptdoc23 <122674408+scriptdoc23@users.noreply.github.com>
2024-12-24 11:11:58 +0100

    reverting changes
    reverting changes i made to teset something

--- napravi_kompaniju.php
+++ napravi_kompaniju.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
      <head>
-        <title>napravi Kompaniju</title>
+        <title>Napravi Kompaniju</title>
         <link rel="stylesheet" href="nav.css">
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
      <head>
-        <title>register</title>
+        <title>Register</title>
         <link rel="stylesheet" href="nav.css">
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
