5a3049dee0df55e2aa314e2848f1c825d88340cc
scriptdoc23 <122674408+scriptdoc23@users.noreply.github.com>
2024-12-24 14:17:20 +0100

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
      <head>
-        <title>login/Register</title>
+        <title>Login/Register</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="nav.css">
      </head>
