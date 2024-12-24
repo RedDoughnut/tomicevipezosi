c1501214bf556cffad6d9e0f5217f427cf3c3d62
scriptdoc23 <122674408+scriptdoc23@users.noreply.github.com>
2024-12-24 11:08:39 +0100

    Update napravi_kompaniju.php

--- napravi_kompaniju.php
+++ napravi_kompaniju.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
      <head>
-        <title>Napravi Kompaniju</title>
+        <title>napravi Kompaniju</title>
         <link rel="stylesheet" href="nav.css">
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
