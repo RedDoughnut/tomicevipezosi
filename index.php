6e5b0a6af7929f7fcfac05b013fd8a56b6f314b6
David Tomić <80597004+Programmers3001@users.noreply.github.com>
2024-12-24 22:06:59 +0100

    font

--- index.php
+++ index.php
@@ -151,7 +151,7 @@ session_start();
                 $res = $conn->query($sql);
                 $res = $res -> fetch_assoc();
                 echo "<li class = 'nav'><a href='user.php' class='nav'><button class='navbut2'>" . $res['firstName'] . "</button></a></li>";
-                echo "<li class = 'nav'>" . $res['balance'] . "T₱</li>";
+                echo "<li class = 'nav' style='font-size: 1rem;'>" . $res['balance'] . "T₱</li>";
               
                 $conn->close();
             }
