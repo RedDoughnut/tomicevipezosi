aaf2cf4d41ccb15cc743f739cfca0c6c70b30d6a
scriptdoc23 <122674408+scriptdoc23@users.noreply.github.com>
2024-12-24 11:10:00 +0100

    Update register.php

--- register.php
+++ register.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
      <head>
-        <title>Register</title>
+        <title>register</title>
         <link rel="stylesheet" href="nav.css">
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
