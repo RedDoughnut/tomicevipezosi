cc47618e3db90d418a194c3e1d9298254fe0d310
David Tomić <80597004+Programmers3001@users.noreply.github.com>
2024-12-24 22:36:11 +0100

    button removal

--- kompanija.php
+++ kompanija.php
@@ -268,7 +268,7 @@ $c = 14;
             <h1 class="label1">Invest</h1>
             <input type="hidden" name="form_id" value="invest">
             <input type="number" name="amount" placeholder="Amount(Stocks)" step="1"><br>
-            <button type="submit">Send request</button><br>
+            <!--<button type="submit">Send request</button><br>-->
         </form>
         <?php
             if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['form_id']) && $_POST['form_id'] == "invest"){
