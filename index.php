5490e36ad4b6706a6e958adab4e3caeeea5088a5
David Tomić <80597004+Programmers3001@users.noreply.github.com>
2024-12-24 22:03:56 +0100

    BALANCE

--- index.php
+++ index.php
@@ -177,10 +177,11 @@ session_start();
                 die('Connection Failed : '.$conn->connect_error);
             }else{
                 $email = $_SESSION['user'];
-                $sql = "SELECT firstName FROM user WHERE email = '$email'";
+                $sql = "SELECT firstName, balance FROM user WHERE email = '$email'";
                 $res = $conn->query($sql);
                 $res = $res -> fetch_assoc();
                 echo "<li class = 'mobile'><a href='user.php'><button class='navbut2'>" . $res['firstName'] . "</button></a></li>";
+                echo "<li class = 'mobile'><a href='user.php'><button class='navbut2'>" . $res['balance'] . "T₱</button></a></li>";
                 $conn->close();
             }
         }
