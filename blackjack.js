44c470cbd8bb0913a794cf3803475d58050b40ba
David Tomić <80597004+Programmers3001@users.noreply.github.com>
2024-12-30 17:43:42 +0100

    aaa

--- blackjack.js
+++ blackjack.js
@@ -84,9 +84,9 @@ document.addEventListener("DOMContentLoaded", function () {
         playerHand.push(dealCard());
         console.log('Player hand after hit:', playerHand);
         console.log('Hand value:', calculateHandValue(playerHand));
+        var val = calculateHandValue(playerHand);
         updateUI();
-        
-        if (calculateHandValue(playerHand)) {
+        if (calculateHandValue(playerHand) > 21) {
             endGame('You busted! Dealer wins');
         }
     }
