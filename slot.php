5ff4d84e77a1250ca2b8399f13cd31569e45c36a
David Tomić <80597004+RedDoughnut@users.noreply.github.com>
2025-06-27 22:51:17 +0200

    a

--- slot.php
+++ slot.php
@@ -368,13 +368,13 @@ $ulogovan = isset($_SESSION["user"]) ? "true" : "false";
         </div>
         <script src="slot.js"></script>
         <button class="button" id="spin" onclick="buttonClick();">Spin!</button>
-        <span>
+        <div>
             <p style="font-size: 1.8rem">Autospin</p>
             <label class="container">
                 <input type="checkbox">
                 <div class="checkmark"></div>
             </label>
-        </span>
+        </div>
         <h1>Kombinacije</h1>
         <h2>1. Five of a kind - 7,500X</h2>
         <p>5 istih brojeva: 11111, 99999, ...</p>
