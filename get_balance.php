d0992012d1233fe95da8f3a75f462268c08a01be
David Tomić <80597004+RedDoughnut@users.noreply.github.com>
2025-06-27 17:53:10 +0200

    a

--- get_balance.php
+++ get_balance.php
@@ -1,12 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start();
 include "SECRETS.php";
-echo $DB_User;
-echo $DB_Pass;
-try{
+
     $conn = new mysqli('sql209.infinityfree.com', $DB_User, $DB_Pass, 'if0_37883576_tomicevipezosi')
 
     // Provera konekcije
@@ -24,17 +19,7 @@ try{
 
     $user_id = $_SESSION["user"];
 
-    $stmt = $conn->prepare("SELECT balance FROM user WHERE email = ?");
-    $stmt->bind_param("i", $user_id);
-    $stmt->execute();
-    $stmt->bind_result($balance);
-    $stmt->fetch();
-    $stmt->close();
-    $conn->close();
-
-    echo $balance;
-}
-catch(mysqli_sql_exception $e){
-    echo $e->getMessage();
-}
+    $res = $conn->query("SELECT balance FROM user WHERE email = ?")->fetch_assoc()[0];
+    
+    echo $res;
 ?>
