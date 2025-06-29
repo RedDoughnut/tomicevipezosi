69992416cb48a3019d1386935b9df059bdbf51d4
David Tomić <80597004+RedDoughnut@users.noreply.github.com>
2025-06-30 01:10:51 +0200

    a

--- kompanija.php
+++ kompanija.php
@@ -291,7 +291,7 @@ error_reporting(E_ALL);
                 }
             }
             $sql = "SELECT history FROM kompanija WHERE id=$id";
-            $labels = array_fill(0, 360, "");
+            $labels = array_fill(0, 72, "");
             $data = json_decode(mysqli_fetch_assoc(mysqli_query($conn, $sql))['history'], true);
             $chart = [
                 "type" => "line",
