852ebad2a9af356cd229f63f5137f945025a64ad
David Tomić <80597004+RedDoughnut@users.noreply.github.com>
2025-06-29 23:07:37 +0200

    a

--- index.php
+++ index.php
@@ -271,7 +271,7 @@ include "SECRETS.php";
                 }
                 $history = json_decode($row["history"], true); 
                 $zaokruzenaCena = number_format($cenaAkcije, 2, '.', '');
-                $history[] = round($cenaAkcije, 2);
+                $history[] = $zaokruzenaCena;
                 $id = $row["id"];
                 if (count($history) > 2160) {
                     array_shift($history);
